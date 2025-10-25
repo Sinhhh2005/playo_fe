57a47611fb4c0ae70f07a457d99c803cc4e1d541
ngo ngoc truong sinh <sinhngo2003@gmail.com>
2025-10-25 19:46:47 +0700

    update

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,10 +1,10 @@
 import AdminLayout from "../pages/admin/AdminLayout";
 import UserManagement from "../components/mainAdmin/UserManagement";
 import AdminDashboard from "../components/mainAdmin/AdminDashboard";
-import FieldManagement from "../components/mainAdmin/FieldManagement";
+// import FieldManagement from "../components/mainAdmin/FieldManagement";
 
 
-import type { FieldRequest } from "../types/field";
+// import type { FieldRequest } from "../types/field";
 
 
 const dummyStats = {
@@ -14,35 +14,35 @@ const dummyStats = {
 	revenue: 15000000,
 };
 
-const initialData: FieldRequest[] = [
-  {
-    id: 1,
-    name: "Sân A",
-    address: "Hà Nội",
-    type: "Bóng đá",
-    status: "Pending",
-    price: 200000,
-    createdAt: "2025-10-21T00:00:00Z",
-  },
-  {
-    id: 2,
-    name: "Sân B",
-    address: "Hải Phòng",
-    type: "Cầu lông",
-    status: "Approved",
-    price: 150000,
-    createdAt: "2025-10-21T00:00:00Z",
-  },
-  {
-    id: 3,
-    name: "Sân C",
-    address: "Đà Nẵng",
-    type: "Bóng rổ",
-    status: "Rejected",
-    price: 250000,
-    createdAt: "2025-10-21T00:00:00Z",
-  },
-];
+// const initialData: FieldRequest[] = [
+//   {
+//     id: 1,
+//     name: "Sân A",
+//     address: "Hà Nội",
+//     type: "Bóng đá",
+//     status: "Pending",
+//     price: 200000,
+//     createdAt: "2025-10-21T00:00:00Z",
+//   },
+//   {
+//     id: 2,
+//     name: "Sân B",
+//     address: "Hải Phòng",
+//     type: "Cầu lông",
+//     status: "Approved",
+//     price: 150000,
+//     createdAt: "2025-10-21T00:00:00Z",
+//   },
+//   {
+//     id: 3,
+//     name: "Sân C",
+//     address: "Đà Nẵng",
+//     type: "Bóng rổ",
+//     status: "Rejected",
+//     price: 250000,
+//     createdAt: "2025-10-21T00:00:00Z",
+//   },
+// ];
 
 
 
@@ -52,7 +52,7 @@ const adminRoutes = {
 	children: [
 		{ path: "adminDashboard", element: <AdminDashboard stats={dummyStats}/> },
 		{ path: "userManagement", element: <UserManagement /> }, 
-		{ path: "fieldManagement", element: <FieldManagement requests={initialData}/> },
+		// { path: "fieldManagement", element: <FieldManagement requests={initialData}/> },
 	],
 };
 
